ae93673ef88c743161258cac9a612e1e25b83a0d
Matthias Pitscher <schaefer9111@gmail.com>
2017-06-11 01:44:58 +0200

    audiotest ver. 2

---
+++ js/audioplayer.js
@@ -0,0 +1,25 @@
+var audio, playbtn;
+function initAudioPlayer(){
+  audio = document.getElementById('guidedMeditation');
+
+  // Set object references
+	playbtn = document.getElementById("playpausebtn");
+
+  // Add Event Handling
+	playbtn.addEventListener("click",playPause);
+
+  // Functions
+	function playPause(){
+		if(audio.paused){
+		    audio.play();
+		    playbtn.style.background = "url(img/Pause.png) no-repeat";
+        playbtn.style.backgroundSize = "contain";
+      } else {
+		    audio.pause();
+		    playbtn.style.background = "url(img/Play.png) no-repeat";
+        playbtn.style.backgroundSize = "contain";
+	    }
+	}
+}
+
+window.addEventListener("load", initAudioPlayer);
